5e381257e4c70f3161e1f32e69677be2b96f4592
DragonDrago <akobirmuso@gmail.com>
2022-07-26 21:15:30 +0500

    Validation Comment

--- WEB_API/DAL/OrderModel.cs
+++ WEB_API/DAL/OrderModel.cs
@@ -6,6 +6,8 @@ namespace WEB_API.DAL
 {
     public class OrderModel
     {
+
+        // Validation !Валидация вынесена на фронтэнд, поэтому я не ставил сюда
         public int Id { get; set; }
         public string Name { get; set; }
         public string CompanyName { get; set; }
